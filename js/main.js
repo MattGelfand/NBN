5815c7e9bca0939f69ff5259122123390f4c62f1
Jeff Escalante <hello@jenius.me>
2014-03-10 23:15:41 -0400

    refactor - thinkful mentor session

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  // set all main panel to window height
+  size_panels();
+
+  // reset panel height on resize
+  $(window).on('resize', size_panels);
+
+  function size_panels(){
+    $('body > div').css('height', window.innerHeight);
+  }
+
+  $(".aside a").on('click', function(e) {
+    e.preventDefault();
+
+    var target = $(this).attr("href");
+
+    $("html, body").stop().animate({
+      scrollLeft: $(target).offset().left,
+      scrollTop: $(target).offset().top
+    }, 500);
+
+  });
+
+});
